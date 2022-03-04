765b65e9cc3cd95978a0bf866284dc0f6b1bddb8
Dillon Nys <nydillon@amazon.com>
2022-03-04 13:18:34 -0700

    Update pull script

--- script/pull.sh
+++ script/pull.sh
@@ -5,8 +5,6 @@
 #
 # It expects the following environment variables to be set:
 #
-#   - AWS_ACCESS_KEY_ID             # Set automatically when using aws-actions/configure-aws-credentials
-#   - AWS_SECRET_ACCESS_KEY         # Set automatically when using aws-actions/configure-aws-credentials
 #   - AWS_REGION                    # The region where you deployed the Amplify project
 #   - AMPLIFY_APP_ID                # The app ID of the Amplify project
 #   - AMPLIFY_ENV_NAME              # The name of the Amplify environment to pull
@@ -22,21 +20,22 @@ IFS='|'
 # Per https://github.com/aws-amplify/amplify-cli/issues/7642#issuecomment-875881203
 # this is the officially correct way to use a session token.
 #
-if [[ -n $CI ]]; then
+if [[ -n $CI && -n $AWS_ACCESS_KEY_ID && -n $AWS_SECRET_ACCESS_KEY ]]; then
 
 mkdir -p ~/.aws
 
+AWS_PROFILE=cicd
 CONFIG_PATH=~/.aws/config
 CREDENTIALS_PATH=~/.aws/credentials
 
 cat <<EOF > $CONFIG_PATH
-[cicd]
+[$AWS_PROFILE]
 region=$AWS_REGION
 output=json
 EOF
 
 cat <<EOF > $CREDENTIALS_PATH
-[cicd]
+[$AWS_PROFILE]
 aws_access_key_id=$AWS_ACCESS_KEY_ID
 aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
 EOF
@@ -55,7 +54,7 @@ FLUTTERCONFIG="{\
 AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"general\",\
 \"useProfile\":true,\
-\"profileName\":\"cicd\",\
+\"profileName\":\"${$AWS_PROFILE:-default}\",\
 \"region\":\"$AWS_REGION\"\
 }"
 AMPLIFY="{\
