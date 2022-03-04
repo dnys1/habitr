e45fd414aa88e60271e44813e8af5ee5b8617b34
Dillon Nys <nydillon@amazon.com>
2022-03-04 11:26:58 -0700

    Add default profile

--- script/pull.sh
+++ script/pull.sh
@@ -2,11 +2,30 @@
 set -e
 IFS='|'
 
+# Create the `default` profile in CI/CD contexts, since the Amplify CLI's 
+# headless mode does not support session tokens.
+if [[ -n $CI ]]; then
+cat <<EOF > ~/.aws/config
+[default]
+region=$AWS_REGION
+output=json
+EOF
+
+cat <<EOF > ~/.aws/credentials
+[default]
+aws_access_key_id=$AWS_ACCESS_KEY_ID
+aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
+aws_session_token=$AWS_SESSION_TOKEN
+EOF
+fi
+
 FLUTTERCONFIG="{\
 \"ResDir\":\"./lib/\",\
 }"
 AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"general\",\
+\"useProfile\":true,\
+\"profileName\":\"default\",\
 \"region\":\"$AWS_REGION\"\
 }"
 AMPLIFY="{\
